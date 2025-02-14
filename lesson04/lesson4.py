949dda08e85f070be497c542cc6369e59105b359
MorganeLQ <momolequere@gmail.com>
2025-02-14 14:59:46 -0500

    Update lesson4.py

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,27 @@
 # Lesson 4
+# Lesson 4
+# Ask for number of fences in each section
+section1 = input("Length of fence in section 1:")
+
+section2 = input("Length of fence in section 2:")
+
+section3 = input("Length of fence in section 3:")
+
+#Calculate total fences to determine how many paint cans
+paintCans = len(section1 + section2 + section3)
+# len = lenght 
+
+#How many paint cans you must buy (by the dozen)
+if (paintCans < 12, paintCans == 12)
+    print ("Number of cans needed: 12")
+elif (paintCans > 12)
+    num = (paintCans//12)
+    print (f"Number of cans needed: {num}")
+
+#How many paint cans left over
+if (num
+
+#How much would it cost
+cost = num * 14.95
+print (f"Cost of paint cans for the dozens: {cost}")
+
