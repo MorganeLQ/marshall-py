48fe18f50bfcb7e508ad9e5195a62f901ec28ca5
MorganeLQ <momolequere@gmail.com>
2025-02-20 12:30:45 -0500

    Update lesson5.py

--- lesson05/lesson5.py
+++ lesson05/lesson5.py
@@ -1,1 +1,14 @@
 # Lesson 5
+#user input amount of money at the start
+money_start = float(input("Amount of money you currently have: ")
+#float changes input to real number
+
+#user input string of cookies
+cookies_sold = print(input("Cookies: ")
+
+#count number of small and big cookies
+big_cookies = cookies_sold.count('b')
+normal_cookies = cookies_sold.count('c')
+
+#output total cookies sold
+total_cookies = big_cookies + normal_cookies
