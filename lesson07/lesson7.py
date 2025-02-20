0d9b8716c3d77de4c77f463640ed5e57b4dbc3ee
MorganeLQ <momolequere@gmail.com>
2025-02-20 13:53:05 -0500

    Update lesson7.py

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,14 @@
 # Lesson 7
+#ask user to input DC
+Dc = int(input(Difficulty class: )
+
+#Generate random number for the dice between 1-20
+import random
+D20 = (random(randrange(1, 20))
+print(f("Dice roll: {D20}")
+
+#output if user succeeds or fails ability check
+if (D20 > Dc)
+	print("player suceeded!")
+else
+	print("player failed! :( ")
