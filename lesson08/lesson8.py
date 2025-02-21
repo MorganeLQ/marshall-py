6bb57488701102a0500a105cac51fd87b3a77c20
MorganeLQ <momolequere@gmail.com>
2025-02-21 11:12:15 -0500

    Update lesson8.py

--- lesson08/lesson8.py
+++ lesson08/lesson8.py
@@ -1,1 +1,18 @@
 # Lesson 8
+#user input
+tournament = input("Tournament wins and loses: ")
+
+#count wins and loses
+wins = tournament.count('W')
+loses = tournament.count('L')
+
+#sort groups
+if (wins > 0)
+	for wins in range(5, 7)
+    	print("Group 1")
+    for wins in range(3, 5)
+    	print("Group 2")
+    for wins in range(1, 3)
+    	print("Group 3")
+else
+	print("Eliminated")
