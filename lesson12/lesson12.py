85aaa7ac0a0e7c5b166ad842a3ce698c4b94f279
MorganeLQ <momolequere@gmail.com>
2025-02-23 13:45:05 -0500

    Update lesson12.py

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -13,7 +13,7 @@ if angle_sum != 180:
 else:
     if angle1 == angle2 == angle3: #chaining operator ==
         print("Equilateral")
-    elif angle1 != angle2 and angle2 != angle3 and angle1 != angle3: #as long as one statement of and is true, print scalene
+    elif angle1 != angle2 and angle2 != angle3 and angle1 != angle3: #as long as all statement of and is true, print scalene
         print("Scalene")
     else:
         print("Isosceles")
