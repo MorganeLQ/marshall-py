8250c0e5a31e8782bfad88c104194fd2956c792a
MorganeLQ <momolequere@gmail.com>
2025-02-23 18:58:40 -0500

    Update lesson22.py

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,18 @@
-# Lesson 22
+#lesson 22
+
+#input
+upper_limit = int(input("Enter N to find the Nth Fibonacci number: "))
+
+#initialize variables
+fib_0 = 0
+fib_1 = 1
+fib_n = 0 #n is currently 2
+
+for n in range(2, upper_limit+1):
+    fib_n = fib_1 + fib_0 #calculate fib(n)
+    
+    #next fib(n)
+    fib_0 = fib_1
+    fib_1 = fib_n
+    
+print(f"Fibonacci({upper_limit}) is {fib_n}.")
