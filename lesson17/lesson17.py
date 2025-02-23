b66e5a57c6a8dccdea2a3a4effef8bbb13bfab9c
MorganeLQ <momolequere@gmail.com>
2025-02-23 15:12:13 -0500

    Update lesson17.py

--- lesson17/lesson17.py
+++ lesson17/lesson17.py
@@ -1,1 +1,12 @@
-# Lesson 17
+#lesson 17
+
+# input
+num = int(input("Enter a number: "))
+
+total_product = 1
+
+for i in range(num, 0, -1): #or range(1, num+1)
+    #range(a, b, steo) --> if step not provided, it is assumed to be 1
+    #i represents each value in sequence
+    total_product *= i
+print(f"Factorial of {num} is {total_product}.")
