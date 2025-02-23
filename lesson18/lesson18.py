18f59b8c33fb128fbf5a5c36b8c4d1289fc6cf16
MorganeLQ <momolequere@gmail.com>
2025-02-23 15:24:10 -0500

    Update lesson18.py

--- lesson18/lesson18.py
+++ lesson18/lesson18.py
@@ -1,1 +1,10 @@
-# Lesson 18
+#lesson 18
+
+#input
+num = int(input("Enter an integer to find factors: "))
+
+divider = 1
+while divider <=  num:
+    print(f"Divider is: {divider}.")
+    
+    divider +=1 #sets new value for divider
