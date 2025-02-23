cb9e27da5299ab0da926cfb583c14aa1d336246c
MorganeLQ <momolequere@gmail.com>
2025-02-23 15:31:40 -0500

    Update lesson18.py

--- lesson18/lesson18.py
+++ lesson18/lesson18.py
@@ -7,4 +7,10 @@ divider = 1
 while divider <=  num:
     print(f"Divider is: {divider}.")
     
+    if num % divider == 0:
+        # if == 0, it is factor, perfectly divisible
+        print(f"{divider} is a factor for {num}.")
+    else:
+        print(f"{divider} is not a factor for {num}.")
+    
     divider +=1 #sets new value for divider
