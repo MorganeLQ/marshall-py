a32bb993d819667045cb8a855e9df532d5f293fa
MorganeLQ <momolequere@gmail.com>
2025-02-23 14:48:52 -0500

    Update lesson16.py

--- lesson16/lesson16.py
+++ lesson16/lesson16.py
@@ -1,1 +1,15 @@
-# Lesson 16
+#lesson 16
+
+#range(a, b, step) --> range from a to b, not includin b, with "step" based interval
+
+#for loop used to look through values in iterable object
+for num in range(1, 51):
+    #num --> variable represent individual item in range
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz!")
+    elif num % 3 == 0:
+        print("Fizz!")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
