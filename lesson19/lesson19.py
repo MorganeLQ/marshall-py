af47a7f08277b3abc0a498f5d6c7748e571f227d
MorganeLQ <momolequere@gmail.com>
2025-02-23 15:48:00 -0500

    Update lesson19.py

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,17 @@
-# Lesson 19
+#lesson 19
+
+#input
+num = int(input("Enter a number to find if it is a prime number: "))
+
+is_prime = True #assumes all number is prime until proven otherwise
+
+for divider in range(2, num):
+    if num % divider == 0:
+        #if divider is a factor, num is not prime
+        is_prime = False
+        break #keyword allows exit from for loop
+
+if is_prime:
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is a composite number.")
