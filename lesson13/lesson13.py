1df658ef7e2d789a88aa88efcd2e7602389e2f12
MorganeLQ <momolequere@gmail.com>
2025-02-23 13:49:19 -0500

    Update lesson13.py

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,20 @@
-# Lesson 13
+#lesson 13
+
+#input
+month = (int(input("Enter month number: ")))
+day = (int(input("Enter day number: ")))
+
+#processing 
+if month == 2 and day == 18:
+    print("special")
+else:
+    if month < 2:
+        print ("Before")
+    elif month > 2:
+        print("After")
+    else:
+        #in the month of february
+        if day < 18:
+            print ("Before")
+        else:
+            print ("After")
