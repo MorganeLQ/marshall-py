1615cb3331f51399a17a5145201af4165f63a2c5
MorganeLQ <momolequere@gmail.com>
2025-02-23 19:36:34 -0500

    Update lesson24.py

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,27 @@
-# Lesson 24
+#lesson 24
+
+#initialize variables
+
+name = '' # empty string for loop
+
+longest_length = 0
+longest_name = ''
+
+while name != 'X':
+    #user input
+    name = input("Enter your name or 'X' to exit:")
+    
+    if name != 'X':
+
+        current_length = len(name)
+    
+        if current_length > longest_length:
+            longest_length = current_length
+            longest_name = name
+    else:
+        print("End of the inputs.")
+
+if longest_name:
+    print(f"The longest name with {longest_length} characters is {longest_name}")
+else:
+    print("Not enough data")
