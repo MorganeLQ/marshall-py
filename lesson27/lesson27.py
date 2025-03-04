52b4555f470cc1031b40f5604d27929ddfc80eed
MorganeLQ <momolequere@gmail.com>
2025-03-04 14:22:19 -0500

    Update lesson27.py

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,16 @@
-# Lesson 27
+#lesson 27
+
+#create function to remove non alphabetical variables from string
+def string_cleaner(text):
+    result = '' #initialize empty string
+    for character in text:
+        #strings are immutable
+        if character.isalpha():
+            #if character is a member of the ACII table
+            result = result + character.lower()
+            #returns in lowercase
+
+    return result
+
+value = string_cleaner(input("Enter text to clean: "))
+print(value)
