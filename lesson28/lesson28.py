1e0a47b4289f758ef8e17520724e0dcca77b6655
MorganeLQ <momolequere@gmail.com>
2025-03-04 14:48:04 -0500

    Update lesson28.py

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,24 @@
-# Lesson 28
+#lesson 28
+
+#palindrome checker
+def palindrome_checker(text):
+    if len(text) < 4:
+        # for string with len smaller than 4, is palyndrome
+        return text[0] == text[-1]
+        #if first and last variable equal, is true
+    else:
+        # text 4 or greater
+        midpoint = len(text) // 2
+        #if len is odd, ignore middle character
+        for i in range(0, midpoint):
+            left = text[i]
+            right = text[(-1*i)-1]
+
+            #check that left and right are equal
+            if left != right:
+                print(f"{text} is not a palindrome")
+                return False #auto terminate loop
+        print(f"{text} is a palindrome")
+        return True
+
+string = palindrome_checker(input("Enter text: "))
