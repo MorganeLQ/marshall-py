7c489cee2d4c6cd324f3ff3bd46c915b578eb62a
MorganeLQ <momolequere@gmail.com>
2025-03-05 14:33:50 -0500

    Update lesson30.py

--- lesson30/lesson30.py
+++ lesson30/lesson30.py
@@ -1,1 +1,19 @@
-# Lesson 30
+#lesson 30
+
+def create_line(num):
+    text = ''
+    for i in range (1, num+1):
+        if i % 2 == 0:
+            #i is even
+            text += '0'
+        else:
+            #i is odd
+            text += '1'
+    return text
+
+def output_pattern(num):
+    for i in range(1, num+1):
+        print(create_line(i))
+
+v = output_pattern(5)
+print(v)
