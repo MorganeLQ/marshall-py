96323e5d3d695065b1641a780a9e75eb5fad889e
MorganeLQ <momolequere@gmail.com>
2025-03-10 22:15:35 -0400

    Update lesson38.py

--- lesson38/lesson38.py
+++ lesson38/lesson38.py
@@ -1,1 +1,16 @@
 # Lesson 38
+
+def is_palindrome(text):
+    return text == text[::-1]
+
+palindromic_numbers = []
+
+for num1 in range(999, 99, -1):
+    for num2 in range(999,99, -1):
+        current_product = num1 + num2
+        
+        if is_palindrome(str(current_product)):
+            #palindromic number
+            palindromic_numbers.append(current_product)
+
+print(f"The largest palindromic number in our list is: {max(palindromic_numbers)}")
