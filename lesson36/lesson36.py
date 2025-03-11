bad1f2577dc983e0ff9ad008fe7d2dc26eb831ea
MorganeLQ <momolequere@gmail.com>
2025-03-10 22:07:09 -0400

    Update lesson36.py

--- lesson36/lesson36.py
+++ lesson36/lesson36.py
@@ -1,1 +1,16 @@
 # Lesson 36
+
+def factors(num):
+    
+    result = []
+    for divider in range(1, num+1):
+        if num % divider == 0:
+            result.append(divider)
+
+def is_prime(num):
+    return len(factors(num)) == 2
+
+print(f'Factors of 13: {factors(13)}')
+print(f'Factors of 36: {factors(36)}')
+print(f'Is 13 prime?: {is_prime(13)}')
+print(f'Is 36 prime?: {is_prime(36)}')
