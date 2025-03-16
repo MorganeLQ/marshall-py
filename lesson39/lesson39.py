e6bec53d913d6d1f3a7ef3d750cc52a3887f3374
MorganeLQ <momolequere@gmail.com>
2025-03-16 19:32:01 -0400

    Update lesson39.py

--- lesson39/lesson39.py
+++ lesson39/lesson39.py
@@ -1,1 +1,12 @@
 # Lesson 39
+
+def gcd(num1, num2):
+    for divider in range(min(num1,num2), 0, -1):
+        if num1 % divider == 0 and num2 % divider == 0:
+            return divider
+    return 1
+
+first = print("Enter first number: ")
+second = print("Enter second number: ")
+
+print(f"The GCD of {first} and {second}: {gcd(first, second)}")
