a8a9d9ed59126049f39b3884a32cbaf87d923bf6
MorganeLQ <momolequere@gmail.com>
2025-03-16 19:43:16 -0400

    Update lesson42.py

--- lesson42/lesson42.py
+++ lesson42/lesson42.py
@@ -1,1 +1,14 @@
 # Lesson 42
+def possible_sum(a_list, target):
+    for i, value in enumerate(a_list):
+        new_target = target - value
+        if new_target in a_list[i+1:]:
+            return True
+            
+    return False
+
+test = [1, 5, 3, 6, 5, 88, 0]
+target = 3
+
+print(f"Can {target} be made from two unique values in: {test}?")
+print(possible_sum(test, target))
